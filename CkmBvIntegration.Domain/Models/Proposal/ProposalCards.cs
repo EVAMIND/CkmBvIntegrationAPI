5bbd550e7968471f9774c4776e05927514e19c8b
Matheus-Evamind <107504356+Matheus-Evamind@users.noreply.github.com>
2024-04-05 22:21:14 -0300

    feat: Proposal domain

---
+++ CkmBvIntegration.Domain/Models/Proposal/ProposalCards.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CkmBvIntegration.Domain.Models.Proposal
+{
+    public class ProposalCards
+    {
+        public string nomeCompleto { get; set; }
+        public string numeroCpf { get; set; }
+        public string dataNascimento { get; set; }
+    }
+}
---
+++ CkmBvIntegration.Infraestructure.BvNet/Repositories/Proposal/ProposalRepository.cs
@@ -0,0 +1,34 @@
+﻿using CkmBvIntegration.Domain.Models.Authentication;
+using CkmBvIntegration.Infraestructure.BvNet.Interfaces.Authentication;
+using CkmBvIntegration.Infraestructure.BvNet.Repositories._Base;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CkmBvIntegration.Infraestructure.BvNet.Repositories.ProposalRepository
+{
+    public class ProposalRepository(
+        IHttpClientFactory httpClientFactory,
+        ILogger<ProposalRepository> logger) : BaseRepository<AuthenticationResponse>(httpClientFactory, "BvTokenAPI", logger), IAuthenticationRepository
+    {
+        private ILogger _logger = logger;
+
+        public async Task<AuthenticationResponse> GenerateTokenAsync(AuthenticationRequest authenticationRequest)
+        {
+            string relativeURL = $"token/id";
+            string jsonResponse;
+            try
+            {
+                jsonResponse = await GetAsync(relativeURL);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(message: "Error when execute GenerateTokenAsync: " + ex.Message);
+                throw;
+            }
+
+   
+            AuthenticationResponse token = JsonConvert.DeserializeObject<AuthenticationResponse>(jsonResponse) ?? new AuthenticationResponse();
+            return token;
+        }
+    }
+}
